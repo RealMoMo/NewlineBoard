30e1e910d71a8ce39d6fdaef8277a3f9d8b44960
炜烨 <czb166@qq.com>
2020-06-30 23:35:16 +0800

    Initial commit

---
+++ app/src/main/java/com/newline/borad/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.newline.borad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gb_main)
+    }
+}
