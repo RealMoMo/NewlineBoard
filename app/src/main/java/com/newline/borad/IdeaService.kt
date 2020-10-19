f601ef4dd182669534632a55db7de026dfa757e8
炜烨 <czb166@qq.com>
2020-10-19 16:46:56 +0800

    [feature] version:1.1.11 1.修改便签的大小

--- app/src/main/java/com/newline/borad/IdeaService.kt
+++ app/src/main/java/com/newline/borad/IdeaService.kt
@@ -37,14 +37,14 @@ class IdeaService : Service(),NoteWidget.NoteGestureListener, NoteWidget.NoteAni
      * NoteView 最小宽度
      */
     private val ideaMinWidth :Int by lazy {
-        (resources.displayMetrics.widthPixels /2.5f).toInt()
+        (resources.displayMetrics.widthPixels *0.566f).toInt()
     }
 
     /**
      * NoteView 最小高度
      */
     private val ideaMinHeight :Int by lazy {
-        (resources.displayMetrics.heightPixels /1.25f).toInt()
+        (resources.displayMetrics.heightPixels *0.566f).toInt()
     }
 
     /**
